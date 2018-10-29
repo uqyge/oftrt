af3f9f6a82c48d41bf575d9e27292ef0e04cf80c
eg <y.ge1222@gmail.com>
2018-10-29 20:24:10 +0100

    test cpp code runner

---
+++ oftrt/temp.cpp
@@ -0,0 +1,20 @@
+#include "csv.h"
+
+// int main()
+// {
+//     io::CSVReader<3> in("ram.csv");
+//     in.read_header(io::ignore_extra_column, "vendor", "size", "speed");
+//     std::string vendor;
+//     int size;
+//     double speed;
+//     while (in.read_row(vendor, size, speed))
+//     {
+//         // do stuff with the data
+//     }
+// }
+#include "iostream"
+int main()
+{
+    std::cout << "hello world!" << '\n';
+    return 1;
+}
